675fb5703c86816da0a0e507c8bc7a07b8bfb5fd
성하빈 <seonghabin@naver.com>
2026-05-27 17:22:12 +0900

    feat: stock 모델 추가

--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    class Market(models.TextChoices):
+        KOSPI = "KOSPI", "유가"
+        KOSDAQ = "KOSDAQ", "코스닥"
+        KONEX = "KONEX", "코넥스"
+
+    stock_code = models.CharField(max_length=20, unique=True) #종목코드
+    stock_name = models.CharField(max_length=100) #회사명
+    market = models.CharField(max_length=10, choices=Market.choices)
+    industry = models.CharField(max_length=100, blank=True, null=True) #업종
+    main_product = models.TextField(blank=True, null=True) #주요제품
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.stock_name}({self.stock_code})"
