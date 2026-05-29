026b80150411c22601da7f41c6c060f19a5c0b5b
성하빈 <seonghabin@naver.com>
2026-05-29 15:41:17 +0900

    feat: 관리자 페이지에 모델 추가

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Stock, Theme, StockTheme
 
-# Register your models here.
+admin.site.register(Stock)
+admin.site.register(Theme)
+admin.site.register(StockTheme)
