c630566ff95d8afc6f48fc6a389c811358207de8
seonghabin <seonghabin@gmail.com>
2026-06-10 13:53:23 +0900

    News 모델 추가

--- news/models.py
+++ news/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    description = models.TextField()
+    content = models.TextField()
+    author = models.CharField(max_length=100, blank=True)
+    publisher = models.CharField(max_length=50)
+    published_at = models.DateTimeField()
+    collected_at = models.DateTimeField(auto_now_add=True)
+    thumbnail_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
