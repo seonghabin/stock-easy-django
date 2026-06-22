c82c913ef7cadd7a1e68bc0a81db94fbfc2431cf
성하빈 <seonghabin@naver.com>
2026-06-22 14:25:03 +0900

    feat: 테마별 관련 뉴스 조회 API 구현

--- news/urls.py
+++ news/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.news_list),
     path("<int:news_id>/", views.news_detail),
     path("stocks/<int:stock_id>/", views.stock_news),
+    path("themes/<int:theme_id>/", views.theme_news),
 ]
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import *
+from stocks.models import StockTheme
 from .serializers import *
 
 
@@ -27,4 +28,16 @@ def stock_news(request, stock_id):
     news_qs = NewsStock.objects.filter(stock_id=stock_id).select_related("news").order_by("-news__published_at")
     news_list = [ns.news for ns in news_qs]
     serializer = NewsSerializer(news_list, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def theme_news(request, theme_id):
+    # 1. 테마에 속한 종목 조회
+    stock_ids = StockTheme.objects.filter(theme_id=theme_id).values_list("stock_id", flat=True)
+    # 2. 해당 종목이 포함된 뉴스 ID 조회
+    news_ids = NewsStock.objects.filter(stock_id__in=stock_ids).values_list("news_id", flat=True)
+    # 3. 뉴스 조회
+    news_qs = News.objects.filter(id__in=news_ids).order_by("-published_at").distinct()
+    
+    serializer = NewsSerializer(news_qs, many=True)
     return Response(serializer.data)
