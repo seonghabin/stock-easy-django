4cc1a8792278bfab96b4f6bb976dc53dcc0f9df3
성하빈 <seonghabin@naver.com>
2026-06-23 16:41:42 +0900

    feat: 기업 검색 api 구현

--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Stock
+from .serializers import StockSerializer
+
+
+@api_view(["GET"])
+def stock_list(request):
+    search = request.GET.get("search")
+
+    stocks = Stock.objects.all().order_by("stock_name")
+
+    if search:
+        stocks = stocks.filter(
+            Q(stock_name__icontains=search) |
+            Q(stock_code__icontains=search)
+        )
+
+    serializer = StockSerializer(stocks, many=True)
+    return Response(serializer.data)
