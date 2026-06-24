d291709761d579ccc1d022c684c4189c7e27f43b
seonghabin <seonghabin@gmail.com>
2026-06-24 20:49:53 +0900

    feat: urts 생성

--- terms/urls.py
+++ terms/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views
+
 
 app_name = "terms"
 
 urlpatterns = [
+    path("my/", views.my_term_list_create),
+    path("my/<int:pk>/", views.my_term_delete),
 ]
