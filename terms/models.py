d36e0ff2ec06c6243c2ebb43c28b0fea82b10db5
seonghabin <seonghabin@gmail.com>
2026-06-24 20:37:51 +0900

    feat: Term, UserTerm 모델 생성

--- terms/models.py
+++ terms/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Term(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserTerm(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="saved_terms")
+    term = models.ForeignKey(Term, on_delete=models.CASCADE, related_name="saved_users")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "term"],name="unique_user_term")
+        ]
+
+    def __str__(self):
+        return f"{self.user} - {self.term.name}"
